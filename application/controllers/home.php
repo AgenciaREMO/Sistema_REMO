943c3a333637146d9f6d09d9a9c00d2899e571a8
Marta <mlgarciao@msn.com>
2016-03-02 14:16:58 -0600

    Incorporacion de los archivos para el uso de codeigniter. Creacion del controlador home y las vistas head,nav y footer. Modificación de la url base.

---
+++ application/controllers/home.php
@@ -0,0 +1,11 @@
+<?php 
+	class Home extends CI_Controller
+	{
+		public function index()
+		{
+			$this->load->view("head");
+			$this->load->view("nav");
+			$this->load->view("footer");
+		}
+	}	
+?>
---
+++ application/views/footer.php
@@ -0,0 +1,11 @@
+        <div class="container">
+            <hr/>
+            <div class="row">
+                <div class="col-md-offset-4 col-md-4 text-center">
+                    <h5>REMO DISE&Ntilde;O E INOVACI&Oacute;N</h5>
+                    <p>&copy; Sistema administrativo 2016</p>      
+                </div><!--/ col -->  
+            </div><!--/ row -->
+        </div><!--/ container -->
+	</body>
+</html>
